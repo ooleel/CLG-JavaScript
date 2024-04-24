c97463acde8d179c4c5e93911eb82c7a873114a5
Leeloo <124119782+ooleel@users.noreply.github.com>
2024-04-24 19:25:44 +1000

    Upload

---
+++ main.js
@@ -0,0 +1,17 @@
+alert("This is not a drill, but an alert!");
+
+console.log("This is a console log message.");
+console.log(1 + 1);
+
+let message = "Peek-a-boo!";
+console.log(message);
+
+document.querySelector(".jslogo").style.transform = "rotate(-15deg)";
+
+// Changes colour when the user clicks on the element
+function colorPop() {
+  let title = document.querySelector(".jsname");
+  console.log(title);
+  title.style.color = "navy";
+  title.style.backgroundColor = "hotpink";
+}
