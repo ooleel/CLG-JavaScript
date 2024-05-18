fb54492dbcbff5d8f579c6e4230e67dd82e21cd4
Leeloo <looloosch@gmail.com>
2024-05-18 11:48:39 +1000

    Project update

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+//My fucntions
+// Changes colour when the user clicks on the element
+function colourPop() {
+  let title = document.querySelector("#topic");
+  console.log(title);
+  title.style.color = "navy";
+  title.style.backgroundColor = "hotpink";
+}
+
+//Top to bottom
 alert("This is not a drill, but an alert!");
 
 console.log("This is a console log message.");
@@ -7,11 +17,3 @@ let message = "Peek-a-boo!";
 console.log(message);
 
 document.querySelector("#logo").style.transform = "rotate(-15deg)";
-
-// Changes colour when the user clicks on the element
-function colorPop() {
-  let title = document.querySelector("#topic");
-  console.log(title);
-  title.style.color = "navy";
-  title.style.backgroundColor = "hotpink";
-}
