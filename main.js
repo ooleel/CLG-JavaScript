80e0e6543a2134af52dd3aeb5874c80e63e88775
Leeloo <looloosch@gmail.com>
2024-05-29 19:28:40 +1000

    conflict

--- main.js
+++ main.js
@@ -17,3 +17,28 @@ let message = "Peek-a-boo!";
 console.log(message);
 
 document.querySelector("#logo").style.transform = "rotate(-15deg)";
+
+//Data types
+const pi = 3.14;
+const euler = 2.7182;
+const golbenNb = 1.6180;
+const speedLight = 299792458;
+console.log("The speed of light is ${speedLight} meters per second.")
+
+// let circleRadius = 4;
+function circlePerimeter(radius){
+  let radius = 4; 
+  let result = radius * 2 * pi;
+}
+circlePerimeter();
+
+let streetFood = "sotteok sotteok";
+let typeFood = "savoury";
+let spicyLevel = "medium";
+console.log("This food is called " + streetFood);
+console.log("It is a ${typeFood} food with a ${spicyLevel} spicy level.");
+console.warn("Be careful, it can be very spicy!");
+console.clear();
+
+setTimeout(console.log("Be careful!"), 5000);
+setTimeout(console.log("Focus!"), 3000); 
